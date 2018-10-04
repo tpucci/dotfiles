f433a9b4728e733c446394ad704bd4e45e00eb09
Thomas Pucci <thomas.lb.pucci@gmail.com>
2018-10-04 09:24:55 +0200

    :sparkles: (zsh) Add more fzf functions

--- zsh/functions/fzf-functions.zsh
+++ zsh/functions/fzf-functions.zsh
@@ -21,3 +21,47 @@ fa() {
                   -o -type d -print 2> /dev/null | fzf +m) &&
   cd "$dir"
 }
+
+# Kill process
+fkil() {
+  local pid
+  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs kill -${1:-9}
+  fi
+}
+
+# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+fb() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+# fshow - git commit browser
+fg() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --reverse --tiebreak=index \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+                FZF-EOF
+              "
+}
+
+# fh - Repeat history, assumes zsh
+fh() {
+  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
+
+# fr - Select commit to rebase onto
+fr() {
+  git log --graph --color=always --format="%h%C(#ff69b4)%d%C(reset) %s" "$@" | fzf --ansi --reverse --tiebreak=index | grep -o '[a-f0-9]\{7\}' | awk '{print $1"^"}' | xargs git rebase -i
+}
+
--- zsh/functions/git-functions.zsh
+++ zsh/functions/git-functions.zsh
@@ -5,3 +5,26 @@ gpr() {
   MESSAGE=$(echo $TITLE'\n' | cat - ./PULL_REQUEST_TEMPLATE.md)
   hub pull-request -m "${MESSAGE}" -b ${2} --browse
 }
+
+# Cd to root dir of git project
+gr() {
+  cd $(git rev-parse --show-toplevel)
+}
+
+# Commit all in a Work in progress commit
+wip() {
+  gr
+  git add .
+  git ci ':construction: Work in progress'
+}
+
+# Unwip: Reset HEAD to previous commit
+unwip() {
+  git reset HEAD~1
+}
+
+# md <folder-name> - Create folder and cd to it
+md(){
+  mkdir "$1"
+  cd "$1"
+}
