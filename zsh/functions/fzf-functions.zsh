d42e4722c79f1b7be975292cd82f19f9cd1ad90a
Thomas Pucci <thomas.lb.pucci@gmail.com>
2019-07-26 10:54:38 +0200

    fix fzf

--- zsh/functions/fzf-functions.zsh
+++ zsh/functions/fzf-functions.zsh
@@ -1,3 +1,21 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+export FZF_COMPLETION_TRIGGER=','
+
+# Custom commands
+# --------------
+
 # Pipe command to fzf. f <cmd>
 f(){
   "$@" | fzf
@@ -55,13 +73,23 @@ fr() {
   git log --graph --color=always --format="%h%C(#ff69b4)%d%C(reset) %s" "$@" | fzf --ansi --reverse --tiebreak=index | grep -o '[a-f0-9]\{7\}' | awk '{print $1"^"}' | xargs git rebase -i
 }
 
-# fy - get yarn commands
-fy() {
-  local yarn_commands yarn_command
-  yarn_commands=$(cat package.json | jq -r '.scripts' | sed -e 's/  "\(.*\)":\s*\(.*\)$/\1|\2/' | sed -e '1d;$d' | column -t -c 2 -s '|') &&
-  yarn_command=$(echo "$yarn_commands" | fzf --reverse --ansi) &&
-  yarn $(echo $yarn_command | awk '{print $1}')
+# yarn - get yarn commands
+_fzf_complete_yarn() {
+  # Get all yarn commands
+  YARN_COMMANDS=$(cat package.json | jq -r '.scripts' | sed -e 's/  "\(.*\)":\s*\(.*\)$/\1|\2/' | sed -e '1d;$d' | column -t -c 2 -s '|')
+
+  ARGS="$@"
+  if [[ $ARGS = 'yarn ' ]]; then
+    _fzf_complete "--reverse -n 1 --height=80%" "$@" < <(
+      echo $YARN_COMMANDS
+    )
+  fi
 }
+_fzf_complete_yarn_post() {
+  # Post-process the fzf output to keep only the command name and not the explanation with it
+  awk '{print $1}'
+}
+[ -n "$BASH" ] && complete -F _fzf_complete_yarn -o default -o bashdefault yarn
 
 # fca - amend in selected commit
 fca() {
