8d459c3707b1da8d62ecbbb391069abf06564145
Thomas Pucci <thomas.lb.pucci@gmail.com>
2020-12-24 10:33:59 +0100

    wip

--- xcode/replaceIDETextKeyBindingSetPlist.sh
+++ xcode/replaceIDETextKeyBindingSetPlist.sh
@@ -1,4 +1,4 @@
 cp /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Versions/A/Resources/IDETextKeyBindingSet.plist xcode/IDETextKeyBindingSet.plist.bak
 git add xcode/IDETextKeyBindingSet.plist.bak
 git ci "backup: xcode/IDETextKeyBindingSet.plist.bak"
-cp xcode/IDETextKeyBindingSet.plist /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Versions/A/Resources/IDETextKeyBindingSet.plist 
+sudo cp xcode/IDETextKeyBindingSet.plist /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Versions/A/Resources/IDETextKeyBindingSet.plist 
